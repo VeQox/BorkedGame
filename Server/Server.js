868083810799f367d08f9674c8bf6fe373cf9b48
VeQox <sonnleitner.luki@gmail.com>
2022-05-29 03:29:43 +0200

    Add Card Generation and User select

--- Server/Server.js
+++ Server/Server.js
@@ -1,11 +1,18 @@
 // Import modules
+import { strictEqual } from "assert";
 import http from "http";
 import WebSocket from "websocket";
 
 const port = 8080;
+const Types = ["♣","♠","♦","♥"];
+const Values = ["2","3","4","5","6","7","8","9","10","B","Q","K","A"];
+
 
 const WebSocketServer = WebSocket.server;
 let Clients = [];
+let UsedCards = [];
+let Stack = [];
+
 
 // Create a http Server
 const httpserver = http.createServer((req, res) => {
@@ -20,6 +27,10 @@ const websocket = new WebSocketServer({
 // Set Events on every "Client" who connects
 websocket.on("request", request => {
     let Connection = request.accept(null, request.origin);
+
+    let cardArray = GetNewHand(10);
+
+    Connection.send(JSON.stringify(cardArray));
 
     Connection.on("close", e => {
         console.log("Client disconnection")
@@ -40,14 +51,33 @@ httpserver.listen(port, () =>{
     console.log(`Server is listening on port ${port}`);
 });
 
-/*
-every5seconds();
-
-// Send a random number to each Client
-function every5seconds(){
-    Clients.forEach(Connection => {
-        Connection.send(Math.random())
+const InitStack = () => {
+    Types.forEach(Type => {
+        Values.forEach(Value => {
+            let Card = {};
+            Card.type = Type;
+            Card.value = Value;
+            Stack.push(Card);
+        });
     });
-    setTimeout(every5seconds, 5000);
 }
-*/
+
+InitStack();
+
+const GetNewHand = (amount) => {
+    let Cards = [];
+    for(let i = 0; i < amount; i++){
+        let Card = GetNewCard();
+        Cards.push(Card);
+        UsedCards.push(Card);
+    }
+    return Cards;
+}
+
+const GetNewCard = () => {
+    let Card = {};
+    do {
+        Card = Stack[Math.floor(Math.random()*Stack.length)];
+    } while (UsedCards.includes(Card));
+    return Card;
+}
